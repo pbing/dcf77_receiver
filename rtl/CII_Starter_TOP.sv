7ceda8fa7898997d074fb6bde1d09aa6dd1e3b49
Bernd Beuster <bernd.beuster@googlemail.com>
2012-03-24 11:02:36 +0100

    Added clock module. Changed HEX display and SW[].

--- rtl/CII_Starter_TOP.sv
+++ rtl/CII_Starter_TOP.sv
@@ -13,10 +13,10 @@ module CII_Starter_TOP (/* Clock Input */
                         input [9:0]   SW,          // Toggle Switch[9:0]
 
                         /* 7-SEG Display */
-                        output [6:0]  HEX0,        // Seven Segment Digit 0
-                        output [6:0]  HEX1,        // Seven Segment Digit 1
-                        output [6:0]  HEX2,        // Seven Segment Digit 2
-                        output [6:0]  HEX3,        // Seven Segment Digit 3
+                        output logic [6:0]  HEX0,  // Seven Segment Digit 0
+                        output logic [6:0]  HEX1,  // Seven Segment Digit 1
+                        output logic [6:0]  HEX2,  // Seven Segment Digit 2
+                        output logic [6:0]  HEX3,  // Seven Segment Digit 3
 
                         /* LED */
                         output [7:0]  LEDG,        // LED Green[7:0]
@@ -29,7 +29,7 @@ module CII_Starter_TOP (/* Clock Input */
                         /* SDRAM Interface */
                         inout  [15:0] DRAM_DQ,     // SDRAM Data bus 16 Bits
                         output [11:0] DRAM_ADDR,   // SDRAM Address bus 12 Bits
-                        output        DRAM_LDQM,   // SDRAM Low-byte Data Mask 
+                        output        DRAM_LDQM,   // SDRAM Low-byte Data Mask
                         output        DRAM_UDQM,   // SDRAM High-byte Data Mask
                         output        DRAM_WE_N,   // SDRAM Write Enable
                         output        DRAM_CAS_N,  // SDRAM Column Address Strobe
@@ -51,8 +51,8 @@ module CII_Starter_TOP (/* Clock Input */
                         /* SRAM Interface */
                         inout  [15:0] SRAM_DQ,     // SRAM Data bus 16 Bits
                         output [17:0] SRAM_ADDR,   // SRAM Address bus 18 Bits
-                        output        SRAM_UB_N,   // SRAM High-byte Data Mask 
-                        output        SRAM_LB_N,   // SRAM Low-byte Data Mask 
+                        output        SRAM_UB_N,   // SRAM High-byte Data Mask
+                        output        SRAM_LB_N,   // SRAM Low-byte Data Mask
                         output        SRAM_WE_N,   // SRAM Write Enable
                         output        SRAM_CE_N,   // SRAM Chip Enable
                         output        SRAM_OE_N,   // SRAM Output Enable
@@ -96,59 +96,133 @@ module CII_Starter_TOP (/* Clock Input */
                         inout [35:0]  GPIO_0,      // GPIO Connection 0
                         inout [35:0]  GPIO_1);     // GPIO Connection 1
 
-   //   Turn off all display
-   //   assign  HEX0            =       '1;
-   //   assign  HEX1            =       '1;
-   //   assign  HEX2            =       '1;
-   //   assign  HEX3            =       '1;
-   //   assign  LEDG            =       8'hFF;
-   //   assign  LEDR            =       10'h3FF;
+   import types::bcd_t;
 
-   //   All inout port turn to tri-state
-   assign       DRAM_DQ         =       16'hzzzz;
-   assign       FL_DQ           =       8'hzz;
-   assign       SRAM_DQ         =       16'hzzzz;
-   assign       SD_DAT          =       1'bz;
-   assign       I2C_SDAT        =       1'bz;
-   assign       AUD_ADCLRCK     =       1'bz;
-   assign       AUD_DACLRCK     =       1'bz;
-   assign       AUD_BCLK        =       1'bz;
-   assign       GPIO_0          =       36'hzzzzzzzzz;
-   assign       GPIO_1          =       36'hzzzzzzzzz;
-
-   wire        rst; 
+   /* common signals */
+   wire        rst;
    wire        clk;
-   wire        rx; 
-   wire [58:0] data_hold;
-   wire        error;
+   wire clk_en_10ms;
+
+   /* DCF77 */
+   wire        dcf77_rx;
+   wire [58:0] dcf77_data;
+   wire        dcf77_error,dcf77_sync;
+
+   /* Clock */
+   /*wire*/ bcd_t [1:0] year,month,day,hour,minute,second;
+   wire [2:0] day_of_week;
+
 
    /* synchronize reset */
    logic [0:1] rst_s;
    always_ff @(posedge clk)
      rst_s<={~KEY[0],rst_s[0]};
-   
+
    assign rst=rst_s[1];
    assign clk=CLOCK_24[0];
+   assign dcf77_rx=GPIO_1[35];
+   assign LEDR[0]=dcf77_error;
+   assign LEDG[0]=dcf77_rx;
    
-   assign rx=GPIO_1[35];
+   clk_en clk_en(.rst(rst),.clk(clk),.clk_en(clk_en_10ms));
 
-   /* DEBUG */
-   assign LEDG[3:0]=data_hold[24:21]; // MM Einer
-   assign LEDG[7:4]=data_hold[28:25]; // MM Zehner
+   dcf77 dcf77(.rst(rst),.clk(clk),.clk_en(clk_en_10ms),
+	       .rx(dcf77_rx),.data_hold(dcf77_data),
+	       .error(dcf77_error),.sync(dcf77_sync));
 
-   assign LEDR[3:0]=data_hold[32:29]; // HH Einer
-   assign LEDR[6:4]=data_hold[35:33]; // HH Zehner
-   assign LEDR[9:7]=data_hold[44:42]; // WW
+   clock clock(.rst(rst),.clk(clk),.clk_en(clk_en_10ms|SW[8]), // SW[8] 0:normal speed, 1:extended speed
+	       .dcf77_sync(dcf77_sync&SW[9]),                  // SW[9] 0:free running  1:sync with DCF77
+	       .dcf77_year(dcf77_data[57:50]),
+	       .dcf77_month({3'b0,dcf77_data[49:45]}),
+	       .dcf77_day({2'b0,dcf77_data[41:36]}),
+	       .dcf77_day_of_week(dcf77_data[44:42]),
+	       .dcf77_hour({2'b0,dcf77_data[34:29]}),
+	       .dcf77_minute({1'b0,dcf77_data[27:21]}),
+	       .year(year),
+	       .month(month),
+	       .day(day),
+	       .day_of_week(day_of_week),
+	       .hour(hour),
+	       .minute(minute),
+	       .second(second));
 
-   //   assign LEDR[3:0]=data_hold[53:50]; // YY Einer
-   //   assign LEDR[7:4]=data_hold[57:54]; // YY Zehner
-   //   assign LEDR[8]=data_hold[58];      // YY parity
+   always_comb
+     priority case(1'b1)
+       SW[0]:
+	 begin
+	    HEX3=bcd_digit(0);
+	    HEX2=bcd_digit(0);
+	    HEX1=bcd_digit(second[1]);
+	    HEX0=bcd_digit(second[0]);
+	 end
 
-   assign HEX3='1;
-   assign HEX2=(error)?~7'b1111001:~7'b1000000;
-   assign HEX1=(error)?~7'b1010000:~7'b1000000;
-   assign HEX0=(error)?~7'b1010000:~7'b1000000;
+       SW[1]:
+	 begin
+	    {HEX3,HEX2}=week_day_chars(day_of_week);
+	    HEX1=bcd_digit(day[1]);
+	    HEX0=bcd_digit(day[0]);
+	 end
 
-   dcf77 dcf77(.rst(rst),.clk(clk),.rx(rx),.data_hold(data_hold),.error(error));
+       SW[2]:
+	 begin
+	    HEX3=bcd_digit(year[1]);
+	    HEX2=bcd_digit(year[0]);
+	    HEX1=bcd_digit(month[1]);
+	    HEX0=bcd_digit(month[0]);
+	 end
 
+       default
+	 begin
+	    HEX3=bcd_digit(hour[1]);
+	    HEX2=bcd_digit(hour[0]);
+	    HEX1=bcd_digit(minute[1]);
+	    HEX0=bcd_digit(minute[0]);
+	 end
+     endcase
+
+
+   /********************************************************************************
+    * Functions
+    ********************************************************************************/
+
+   /* HEX display digit
+    *      A
+    *    F   B
+    *      G
+    *    E   C
+    *      D
+    */
+   function [6:0] bcd_digit(input bcd_t i);
+      bit [6:0] digit['h10]='{/*  GFEDCBA */
+			      ~7'b0111111,  // 0
+			      ~7'b0000110,  // 1
+			      ~7'b1011011,  // 2
+			      ~7'b1001111,  // 3
+			      ~7'b1100110,  // 4
+			      ~7'b1101101,  // 5
+			      ~7'b1111101,  // 6
+			      ~7'b0000111,  // 7
+			      ~7'b1111111,  // 8
+			      ~7'b1101111,  // 9
+			      ~7'b1110111,  // A
+			      ~7'b1111100,  // b
+			      ~7'b0111001,  // C
+			      ~7'b1011110,  // d
+			      ~7'b1111001,  // E
+			      ~7'b1110001}; // F
+      bcd_digit=digit[i];
+   endfunction
+
+   function [13:0] week_day_chars(input [2:0] i);
+      bit [13:0] week_day[8]='{/*   GFEDCBA_GFEDCBA */
+			       ~14'b1000000_1000000,  // --
+			       ~14'b0110111_1011100,  // Mo
+			       ~14'b1111000_0011100,  // tu
+			       ~14'b0111110_1111001,  // WE
+			       ~14'b1111000_1110100,  // th
+			       ~14'b1110001_1010000,  // Fr
+			       ~14'b1101101_1110111,  // SA
+                               ~14'b1101101_1011100}; // So
+      week_day_chars=week_day[i];
+   endfunction
 endmodule
