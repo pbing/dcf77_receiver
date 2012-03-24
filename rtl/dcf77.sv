9fc82f15a2f49aaff7b88eb999db2dc5a5e07411
Bernd Beuster <bernd.beuster@googlemail.com>
2012-03-24 11:05:47 +0100

    Use port 'sync' instead of port 'error'. Improved plausibilty detection. Use external 10 ms clock enable.
    Signed-off-by: Bernd Beuster <bernd.beuster@googlemail.com>

--- rtl/dcf77.sv
+++ rtl/dcf77.sv
@@ -2,36 +2,25 @@
 
 module dcf77(input               rst,       // reset
 	     input               clk,       // clock (24 MHz)
+	     input               clk_en,    // clock enable (10 ms)
 	     input               rx,        // pulse 100 ms=0, pulse 200 ms=1
 	     output logic [58:0] data_hold, // data hold register
-	     output logic        error);    // error flag
+	     output logic        error,     // error flag
+	     output logic        sync);     // synchronize clock
 
-   logic [17:0] count_10ms;
-   logic        en_10ms;
-   
-   /* 10ms*/
-   always_ff @(posedge clk)
-     if(rst)
-       count_10ms<='0;
-     else if(count_10ms==18'd239999)
-       count_10ms<='0;
-     else
-       count_10ms<=count_10ms+18'd1;
-   
-   always_comb
-     en_10ms=(count_10ms==18'd239999);
+   logic [58:0] data_shift;       // data shift register
+   logic        valid,data_valid; // received data frame is valid
 
    always_ff @(posedge clk)
      begin:main
 	logic [0:3]  rx_s;                // [0:1]=synchronization, [1:3]=majority
 	logic [0:1]  rx_d;                // rx data pipe
-	logic [58:0] data_shift;          // data shift register
-	logic [4:0]  counter_pulse;       // counter for pulse width
+	logic [4:0]  counter_pulse;       // counter for pulse width (0=5'd10, 1=5'd20)
 	logic        d;                   // serial data
 	logic        shift_en;            // shift enable
 	logic [7:0]  counter_start[1:2];  // counter for start detection
 	logic        start;               // start of frame
-	
+
 	if(rst)
 	  begin
 	     rx_s<='0;
@@ -44,8 +33,10 @@ module dcf77(input               rst,       // reset
 	     start<='0;
 	     data_shift<='0;
 	     data_hold<='0;
+	     error<='1;
+	     data_valid<='0;
 	  end
-	else if(en_10ms)
+	else if(clk_en)
 	  begin
 	     /* synchronization */
 	     rx_s<={rx,rx_s[0:2]};
@@ -53,7 +44,7 @@ module dcf77(input               rst,       // reset
 	     /* 2-of-3 majority */
 	     rx_d[0]<=(rx_s[1] && rx_s[2]) || (rx_s[2] && rx_s[3]) || (rx_s[1] && rx_s[3]);
 	     rx_d[1]<=rx_d[0];
-	     
+
 	     /* detect 0/1 */
 	     shift_en<=1'b0;
 
@@ -61,15 +52,15 @@ module dcf77(input               rst,       // reset
 	       counter_pulse<=counter_pulse+5'd1;
 	     else if(rx_d==2'b01) // falling edge
 	       begin
-		  d<=(counter_pulse<'d15)?1'b0:1'b1;
+		  d<=(counter_pulse<'d15)?1'b0:1'b1; // Threshold is 0.15 seconds.
 		  shift_en<=1'b1;
 		  counter_pulse<='0;
 	       end
-	     
+
 	     /* detect start */
-	     start<=1'b0;
 	     counter_start[1]<=counter_start[1]+8'd1;
-	     
+	     start<=1'b0;
+
 	     if(rx_d==2'b10) // rising edge
 	       begin
 		  start<=(counter_start[1]-counter_start[2])>8'd150; // Delay greater than 1.5 seconds?
@@ -79,31 +70,49 @@ module dcf77(input               rst,       // reset
 	     /* shift register */
 	     if(shift_en)
 	       data_shift<={d,data_shift[58:1]};
-	     
-	     if(start) 
-	       data_hold<=data_shift;
+
+	     /* hold register,error flag, data valid */
+	     data_valid<=1'b0;
+
+	     if(start)
+	       begin
+		  data_hold<=data_shift;
+		  error<=!valid;
+		  data_valid<=valid;
+	       end
 	  end
      end
-   
-   /* error detection */
+
+   /* synchronization to external modules */
+   always_comb sync=data_valid & clk_en;
+
+   /* check validy of DCF77 frame */
    always_comb
-     begin:error_detection
-	logic p1,p2,p3;                   // parity information
-	logic minutes,hours,day_of_month, // check validy
+     begin:validy_check
+	logic p1,p2,p3;                   // parity flags
+	logic start_of_minute,            // validy flags
+	      start_of_encoding_times,
+	      minutes,hours,day_of_month,
 	      day_of_week,month,year;
-	
-	p1=~^data_hold[28:21]; // even parity over minutes
-	p2=~^data_hold[35:29]; // even parity over hours
-	p3=~^data_hold[58:36]; // even parity over date
 
-	minutes     =(data_hold[27:25]<6 && data_hold[24:21]<10); 
-	hours       =(data_hold[34:33]<3 && data_hold[32:29]<10); 
-	day_of_month=(data_hold[41:40]<3 && data_hold[39:36]<10); 
-	day_of_week =(data_hold[44:42]>0 && data_hold[44:42]<8);
-	month       =(data_hold[48:45]<10);
-	year        =(data_hold[57:54]<10 && data_hold[53:50]<10); 
+	p1=~^data_shift[28:21]; // even parity over minutes
+	p2=~^data_shift[35:29]; // even parity over hours
+	p3=~^data_shift[58:36]; // even parity over date
 
-	error=!(p1 && p2 && p3 && 
-		minutes && hours && day_of_month && day_of_week && month && year);
+	/* check validy */
+	start_of_minute=!data_shift[0];
+	start_of_encoding_times=data_shift[20];
+	minutes     =(data_shift[27:25]<6 && data_shift[24:21]<10);
+	hours       =(data_shift[34:33]<3 && data_shift[32:29]<10 && !(data_shift[34:33]==2 && data_shift[32:29]>3));
+	day_of_month=(data_shift[39:36]<10 && !(data_shift[41:40]==3 && data_shift[39:36]>1));
+	day_of_week =(data_shift[44:42]>0 && data_shift[44:42]<8);
+	month       =(data_shift[48:45]<10 && !(data_shift[49]==1 && data_shift[48:45]>2));
+	year        =(data_shift[57:54]<10 && data_shift[53:50]<10);
+
+	valid=(p1 && p2 && p3 &&
+	       start_of_minute && start_of_encoding_times &&
+	       minutes && hours &&
+	       day_of_month && day_of_week &&
+	       month && year);
      end
 endmodule
