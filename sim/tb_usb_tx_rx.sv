f3a050fbb3815a662cea04012cdbf2e290bd2a66
Bernd Beuster <bernd.beuster@googlemail.com>
2012-11-18 17:11:38 +0100

    Changed clock frequncy of 24 MHz.

--- sim/tb_usb_tx_rx.sv
+++ sim/tb_usb_tx_rx.sv
@@ -1,46 +1,54 @@
-/* Testbench USB-TX/RX */
+/* Testbench USB-TX/RX 
+ * RX is connected with TX.
+ */
 
 module tb_usb_tx_rx;
    timeunit 1ns;
    timeprecision 1ps;
 
    const realtime tusb=1s/1.5e6, // low speed
-		  tclk=tusb/4;
-
+		  tclk=1s/24.0e6;
 
    import types::*;
 
-   bit         reset=1;
-   bit         clk;
+   bit          reset=1'b1;
+   bit          clk;
+
    d_port_t    d;
+   d_port_t    line_state;
 
    logic [7:0] tx_data;
    bit         tx_valid;
    wire        tx_ready;
-
+	       
+   wire        rx_clk_en;
+   d_port_t    rxd;
    wire [7:0]  rx_data;
    wire        rx_active,rx_valid,rx_error;
-   d_port_t    rx_line_state;
+
+   cdr    cdr(.*,.q(rxd),.strobe(rx_clk_en));
+   
+   usb_rx usb_rx(.*,.clk_en(rx_clk_en),.data(rx_data),.active(rx_active),.valid(rx_valid),.error(rx_error));
 
    usb_tx usb_tx(.*,.data(tx_data),.valid(tx_valid),.ready(tx_ready));
 
-   usb_rx usb_rx(.*,.data(rx_data),.active(rx_active),.valid(rx_valid),.error(rx_error),.line_state(rx_line_state));
-
    initial forever #(tclk/2) clk=~clk;
+
+   always @(posedge clk)
+     if(tx_ready)
+       tx_data=$random;
 
    initial
      begin
 	repeat(3) @(posedge clk);
-	reset=0;
+	reset=1'b0;
 
-	#1us;
-	tx_data=8'h01; tx_valid=1;
+	repeat(10) @(posedge clk);
+	tx_valid<=1'b1; tx_data<=8'hc3;
+	repeat(16*8*100) @(posedge clk);
+	tx_valid=1'b0;
 
-	do @(posedge clk); while(!tx_ready);
-	tx_data='z;
-	tx_valid=0;
-
-	repeat(100) @(posedge clk);
-	#100ns $stop;
+	repeat(16*8) @(posedge clk);
+	$stop;
      end
 endmodule
