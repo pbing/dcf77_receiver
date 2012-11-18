3f2206677367fcb26d03043f816feb250b72d952
Bernd Beuster <bernd.beuster@googlemail.com>
2012-11-18 15:12:35 +0100

    Changed to common clock frequency of 24 MHz.

--- rtl/usb_tx.sv
+++ rtl/usb_tx.sv
@@ -1,55 +1,52 @@
-/* USB low/full speed sender */
+/* USB Low Speed sender */
 
 module usb_tx
-   import types::*;
-   (input        reset,      // reset
-    input        clk,        // system clock (low speed: 6 MHz, full speed: 48 MHz)
-    output       d_port_t d, // USB port D+,D-
-    input [7:0]  data,       // data from SIE
-    input        valid,      // rise:SYNC,1:send data,fall:EOP
-    output logic ready);     // data has been sent
+  import types::*;
+   (input           reset,  // reset
+    input           clk,    // system clock (24 MHz)
+    output d_port_t d,      // USB port D+,D-
+    input  [7:0]    data,   // data from SIE
+    input           valid,  // rise:SYNC,1:send data,fall:EOP
+    output logic    ready); // data has been sent
 
+   /* bit/byte enable */
+   logic [3:0] bit_counter;  // 24 MHz/1.5 MHz=16
+   logic [2:0] byte_counter; // 8 bits per byte
+   logic       stuffing;     // bit stuffing
+   logic [2:0] num_ones;     // number of ones
+   logic       en_bit;       // enable bit
 
-   /* bit/byte clock */
-   logic       stuffing;
-   logic [2:0] num_ones;
-   logic [1:0] bit_counter; // bit time=4 clocks
-   logic [2:0] byte_counter;
-   logic       en_bit_byte_counter;
+   enum logic [2:0] {RESET,TX_WAIT,SEND_SYNC,TX_DATA_LOAD,TX_DATA_WAIT,SEND_EOP} tx_state,tx_next;
 
-   always_comb stuffing=(num_ones=='d6);
-   
    always_ff @(posedge clk)
-     if(reset || !en_bit_byte_counter)
+     if(reset)
        begin
-	  bit_counter<='0;
-	  byte_counter<='0;
+	  bit_counter <=4'd0;
+	  byte_counter<=3'd0;
        end
      else
        begin
-	  bit_counter<=bit_counter+2'd1;
-	  if(bit_counter=='1 && !stuffing)
-	    byte_counter<=byte_counter+3'd1;
+	  if(tx_state==RESET || (!valid && tx_state==TX_WAIT))
+	    begin
+	       bit_counter <=4'd0;
+	       byte_counter<=3'd0;
+	    end
+	  else
+	    begin
+	       bit_counter<=bit_counter+2'd1;
+
+	       if(bit_counter==4'd15 && !stuffing)
+		 byte_counter<=byte_counter+3'd1;
+	    end
        end
 
-   logic en_bit,en_byte;
-   
    always_comb
      begin
-        en_bit=(bit_counter=='1);
-	en_byte=(byte_counter=='1 && en_bit);
+        en_bit=(tx_state!=RESET && tx_state!=TX_WAIT && bit_counter==4'd0);
+	ready =(en_bit && byte_counter==3'd7);
      end
 
-   /*************************************************************
-    * TX FSM
-    *
-    * Use dummy states to get a state number of powers of two.
-    * Otherwise the synthesis software of Quartus will insert a
-    * modulo-N divider because of tx_state.next().
-    *************************************************************/
-   enum logic [4:0] {RESET,TX_WAIT,SEND_SYNC[8],TX_DATA_LOAD,TX_DATA_WAIT[8],SEND_EOP[11],DUMMY[2]} tx_state,tx_next;
-   logic idle,eop;
-   
+   /* TX FSM */
    always_ff @(posedge clk)
      if(reset)
        tx_state<=RESET;
@@ -58,115 +55,75 @@ module usb_tx
 
    always_comb
      begin
-	ready=1'b0;
-	idle=1'b0;
-	eop=1'b0;
-	en_bit_byte_counter=1'b1;
 	tx_next=tx_state;
 
 	case(tx_state)
 	  RESET:
-	    begin
-	       idle=1'b1;
-	       en_bit_byte_counter=1'b0;
-	       if(!reset) tx_next=TX_WAIT;
-	    end
-	  
+	    if(!reset) tx_next=TX_WAIT;
+
 	  TX_WAIT:
-	    begin
-	       idle=1'b1;
-	       en_bit_byte_counter=1'b0;
-	       if(valid)
-		 begin
-		    en_bit_byte_counter=1'b1;
-		    tx_next=SEND_SYNC0;
-		 end
-	    end
+	    if(valid) tx_next=SEND_SYNC;
 
-	  SEND_SYNC0:
-	    if(en_bit)
-	      begin
-		 ready=1'b1;
-		 tx_next=tx_state.next();
-	      end
-
-	  SEND_SYNC1,SEND_SYNC2,SEND_SYNC3,
-	    SEND_SYNC4,SEND_SYNC5,SEND_SYNC6,SEND_SYNC7:
-	      if(en_bit) tx_next=tx_state.next();
+	  SEND_SYNC:
+	    if(ready) tx_next=TX_DATA_LOAD;
 
 	  TX_DATA_LOAD:
 	    begin
-	       if(valid)
-		 begin
-		    ready=1'b1;
-		    tx_next=TX_DATA_WAIT0;
-		 end
-	       else
-		 if(en_bit) tx_next=SEND_EOP0;
+	       if(valid && en_bit)              tx_next=TX_DATA_WAIT;
+	       if(!valid && byte_counter==3'd1) tx_next=SEND_EOP;
 	    end
 
-	  TX_DATA_WAIT0,TX_DATA_WAIT1,TX_DATA_WAIT2,TX_DATA_WAIT3,
-	    TX_DATA_WAIT4,TX_DATA_WAIT5,TX_DATA_WAIT6:
-	      if(en_bit) tx_next=tx_state.next();
+	  TX_DATA_WAIT:
+	    if(ready)
+	      tx_next=TX_DATA_LOAD;
 
-	  TX_DATA_WAIT7:
-	    if(en_bit) tx_next=TX_DATA_LOAD;
+	  SEND_EOP:
+	    if(en_bit && byte_counter==3'd3) tx_next=TX_WAIT;
 
-	  SEND_EOP0:
-	      begin
-		 eop=1'b1;
-		 if(en_bit) tx_next=tx_state.next();
-	      end
-	  
-	  SEND_EOP1:
-	      begin
-		 eop=1'b1;
-		 if(en_bit) tx_next=TX_WAIT;
-	      end
-	  
 	  default tx_next=RESET;
 	endcase
      end
 
    /* TX load/shift register */
    logic [7:0] tx_load,tx_shift;
-   logic       tx_bit;
 
    always_ff @(posedge clk)
      if(reset)
-       tx_load<='0;
+       tx_load<=8'b0;
      else if(ready)
        tx_load<=data;
 
    always_ff @(posedge clk)
      if(reset)
-       tx_shift<='0;
+       tx_shift<=8'b0;
      else
-       if(tx_state==TX_WAIT)
-	 tx_shift<=8'h80; // SYNC pattern
-       else if(en_byte) // FIXME
+       if(valid && tx_state==TX_WAIT)
+	 tx_shift<=8'b10000000; // SYNC pattern
+       else if(en_bit && tx_state==TX_DATA_LOAD)
 	 tx_shift<=tx_load;
-       else if(tx_bit)
+       else if(en_bit && !stuffing)
 	 tx_shift<={1'bx,tx_shift[7-:7]};
-
 
    /* bit stuffing */
    logic tx_serial;
-   
+
    always_ff @(posedge clk) // FIXME
      if(reset)
-       num_ones<='0;
+       num_ones<=3'd0;
      else if(en_bit)
        if(tx_shift[0])
 	 if(stuffing)
-	   num_ones<='0;
+	   num_ones<=3'd0;
 	 else
 	   num_ones<=num_ones+3'd1;
        else
-	 num_ones<='0;
+	 num_ones<=3'd0;
 
-   always_comb tx_bit=(en_bit && !stuffing);
-   always_comb tx_serial=(stuffing)?1'b0:tx_shift[0];
+   always_comb
+     begin
+	stuffing =(num_ones==3'd6);
+	tx_serial=(stuffing)?1'b0:tx_shift[0];
+     end
 
    /* NRZI coding */
    logic nrzi;
@@ -179,9 +136,9 @@ module usb_tx
 
    /* assign D+,D- */
    always_comb
-     if(eop)
+     if(tx_state==SEND_EOP)
        d=SE0;
-     else if(idle)
+     else if(tx_state==TX_WAIT)
        d=J;
      else
        d=(nrzi)?K:J;
--- sim/tb_usb_tx.sv
+++ sim/tb_usb_tx.sv
@@ -5,13 +5,13 @@ module tb_usb_tx;
    timeprecision 1ps;
 
    const realtime tusb=1s/1.5e6, // low speed
-		  tclk=tusb/4;
-
+		  tclk=1s/24.0e6;
 
    import types::*;
 
    bit           reset=1;
    bit           clk;
+   bit           clk_en;
    var d_port_t  d;
    bit [7:0]     data;
    bit           valid;
@@ -30,14 +30,19 @@ module tb_usb_tx;
    initial
      begin
 	repeat(3) @(posedge clk);
-	reset=0;
+	reset=1'b0;
 
 	repeat(10) @(posedge clk);
-	valid=1;
-	repeat(4*8*5) @(posedge clk);
-	valid=0;
+	valid<=1'b1; pid(DATA0);
+	repeat(16*8*30) @(posedge clk);
+	valid=1'b0;
 
-	repeat(30) @(posedge clk);
-	#3us $stop;
+	repeat(16*8) @(posedge clk);
+	$stop;
      end
+
+   task pid(pid_t x);
+      data<={x,~x};
+   endtask
+
 endmodule
