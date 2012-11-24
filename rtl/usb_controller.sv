d91e2d0cd640d9378f9396b8087f06fe81d1a2cd
Bernd Beuster <bernd.beuster@googlemail.com>
2012-11-24 18:23:09 +0100

    Fixed CRC5 calculation.

--- rtl/usb_controller.sv
+++ rtl/usb_controller.sv
@@ -2,52 +2,93 @@
 
 module usb_controller
   import types::*;
-   (input          reset,      // system reset
-    input          clk,        // system clock (24 MHz)
+   (input              reset,      // system reset
+    input              clk,        // system clock (24 MHz)
 
     /* USB Bus */
-    input d_port_t line_state, // synchronized D+,D-
+    input     d_port_t line_state, // synchronized D+,D-
 
     /* TX */
-    output [7:0]   tx_data,    // data from SIE
-    output         tx_valid,   // rise:SYNC,1:send data,fall:EOP
-    input          tx_ready,   // data has been sent
+    output       [7:0] tx_data,    // data from SIE
+    output             tx_valid,   // rise:SYNC,1:send data,fall:EOP
+    input              tx_ready,   // data has been sent
 
     /* RX */
-    input  [7:0]   rx_data,    // data to SIE
-    input          rx_active,  // active between SYNC und EOP
-    input          rx_valid,   // data valid pulse
-    input          rx_error);  // error detected
+    input        [7:0] rx_data,    // data to SIE
+    input              rx_active,  // active between SYNC und EOP
+    input              rx_valid,   // data valid pulse
+    input              rx_error,   // error detected
 
-   enum logic [31:0] {IDLE,TOKEN[3]} state,next;
+    /* Device */
+    output logic [6:0] address,    // device address
+    output logic [3:0] end_point); // end point
+
+   enum logic [31:0] {IDLE,TOKEN[3]} state;
 
    /* FIXME */
    assign tx_data =8'h0;
    assign tx_valid=1'b0;
 
    (* noprune *) var   pid_t pid;
-   (* noprune *) logic [6:0] address;
-   (* noprune *) logic [3:0] end_point;
-   (* noprune *) logic       flag_crc5;
+   (* noprune *) logic [4:0] crc5;
 
    always_ff @(posedge clk)
      if(reset)
        begin
-	  address  <='0;
-	  end_point<='0;
-	  flag_crc5<='0;
+	  state    <=IDLE;
+	  pid      <=pid_t'('0);
+	  address  <=7'b0;
+	  end_point<=4'b0;
+	  crc5     <=5'b0;
        end
-     else if(rx_valid)
-       begin
-	  if(valid_pid(rx_data))
-	    pid<=pid_t'(rx_data[3:0]);
+     else
+       case(state)
+	 IDLE:
+	   if(rx_valid && !rx_error)
+	     if(valid_pid(rx_data))
+	       begin
+		  var pid_t current_pid;
 
-	  if(pid==IN)
-	    begin
-	       address<=rx_data[6:0];
-	    end
-       end
+		  current_pid=pid_t'(rx_data[3:0]);
+		  pid       <=current_pid;
 
+		  case(current_pid)
+		    OUT,IN,SETUP: state<=TOKEN0;
+		  endcase
+	       end
+	     else
+	       pid<=pid_t'('0);
+
+	 TOKEN0:
+	   if(rx_valid)
+	     if(!rx_error)
+	       begin
+		  address     <=rx_data[6:0];
+		  end_point[0]<=rx_data[7];
+		  state       <=TOKEN1;
+	       end
+	     else
+	       state<=IDLE;
+
+	 TOKEN1:
+	   if(rx_valid)
+	     if(!rx_error)
+	       begin
+		  end_point[3:1]<=rx_data[2:0];
+		  crc5          <=rx_data[7:3];
+
+		  if(valid_crc5({rx_data,end_point[3],address}))
+		    state<=TOKEN2; // FIXME
+		  else
+		    state<=IDLE;
+	       end
+	     else
+	       state<=IDLE;
+       endcase
+
+   /************************************************************************
+    * Functions
+    ************************************************************************/
 
    function valid_pid(input [7:0] d);
       valid_pid=(d[3:0] == ~d[7:4]);
@@ -60,18 +101,18 @@ module usb_controller
     * be 5'b01100.
     */
    function valid_crc5(input [15:0] d);
-      const bit [4:0] crc5_poly=5'b00101,
-		      crc5_res =5'b01100;
+      const bit [4:0] crc5_poly=5'b10100, // inverse direction
+		      crc5_res =5'b00110; // inverse direction
       logic [4:0] crc5;
 
       crc5='1;
 
-      for(int i=$low(d);i<=$high(d);i++)
-	if(crc5[$left(crc5)]^d[i])
-	  crc5=(crc5<<1)^crc5_poly;
+      for(int i=$right(d);i<=$left(d);i++)
+	if(crc5[$right(crc5)]^d[i])
+	  crc5=(crc5>>1)^crc5_poly;
 	else
-	  crc5=crc5<<1;
+	  crc5=crc5>>1;
 
-      valid_crc5=(crc5_res == ~crc5);
+      valid_crc5=(crc5_res == crc5);
    endfunction
 endmodule
